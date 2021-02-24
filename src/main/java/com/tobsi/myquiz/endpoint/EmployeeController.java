47925ca3555f9ce5a1de7420c4e6801a5e51ebc3
Tobias Ellmauer <tobias.ellmauer@roiderhof.at>
2021-02-24 17:12:30 +0100

    fix annotations

--- src/main/java/com/tobsi/myquiz/endpoint/EmployeeController.java
+++ src/main/java/com/tobsi/myquiz/endpoint/EmployeeController.java
@@ -5,15 +5,10 @@ import java.util.List;
 import com.tobsi.myquiz.endpoint.dto.EmployeeDto;
 import com.tobsi.myquiz.endpoint.mapper.EmployeeMapper;
 import com.tobsi.myquiz.service.EmployeeService;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 class EmployeeController {
 
     private final EmployeeService service;
