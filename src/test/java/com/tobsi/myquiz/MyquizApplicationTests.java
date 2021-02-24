2b98b3e6d24ab9e4d4004423b6316bcd28c93e36
Tobias Ellmauer <tobias.ellmauer@roiderhof.at>
2021-02-24 12:50:48 +0100

    Initial commit

---
+++ src/test/java/com/tobsi/myquiz/MyquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobsi.myquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyquizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
